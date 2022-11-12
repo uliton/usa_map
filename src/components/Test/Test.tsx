cd070b8ed158e75e9ae9201b93dee91a0823aef9
Oleksandr Kharutsa <al.haruca@gmail.com>
2022-11-12 23:08:14 +0100

    map

--- src/components/Test/Test.tsx
+++ src/components/Test/Test.tsx
@@ -1,17 +1,52 @@
-import React from "react";
-import { MapUSA } from "../../ui/MapUSA";
-import { MapCanada } from "../../ui/MapCanada";
+import React, { useState } from "react";
+import states from "../../framework/states.json";
+import { MapUSA } from "../MapUSA";
 
+import './Test.scss';
 
 export const Test: React.FC = () => {
+  const [countyAbbreviation, setCountyAbbreviation] = useState('');
+
+  const stateInfo = states.find(state => state.abbreviation === countyAbbreviation) || '';
+  // console.log(stateInfo);
+
   // #223C2B green
   // #C4C4C4 grey
   return (
-    <div>
-      {/* <MapCanada /> */}
+    <div className="test">
+      <div className="test__map">
+        <MapUSA
+          countyId={countyAbbreviation}
+          setCountyId={setCountyAbbreviation}
+        />
+      </div>
 
-      <div>
-        
+      <div className="test__info">
+        {stateInfo 
+          ? (
+            <div className="info">
+              <span className="info--state">
+                {stateInfo.state}
+              </span>
+
+              <span className="info--capital">
+                {stateInfo.capital.name}
+              </span>
+
+              <span className="info">
+                coordinate: {stateInfo.capital.lat}, {stateInfo.capital.long}
+              </span>
+
+              <span className="info--fun">
+                {stateInfo.fun}
+              </span>
+            </div>
+          )
+          : (
+            <div className="info">
+              Choose state to see info
+            </div>
+          )}
       </div>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { store } from './store';
-import { Provider } from 'react-redux';
+
+
 import { HashRouter } from 'react-router-dom';
 import { App } from './App';
 import './index.scss';
@@ -11,10 +11,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    
       <HashRouter>
         <App />
       </HashRouter>
-    </Provider>
+
   </React.StrictMode>
 );
